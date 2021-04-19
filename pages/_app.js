324ce6b0999da36bc609a0eaf096a7c6b5b964a8
Remy Oudemans <remyoudemans@gmail.com>
2021-04-19 11:54:27 +0100

    fix: apply box-sizing reset to all elements

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
   }
 `
 
